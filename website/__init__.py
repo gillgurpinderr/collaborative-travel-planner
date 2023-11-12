f2d85c1f5369ce4a3439f041489740dfd7556659
anuragcar <akar01@nyit.edu>
2023-11-12 18:32:11 -0500

    database draft

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,9 @@
 from os import path
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -8,9 +11,10 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'h'
-    app.config['SQLALCHEMY_DATABASE_URI'] =f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-    
+    migrate = Migrate(app, db)
+
     from .auth import auth
     app.register_blueprint(auth, url_prefix='/')
     
@@ -23,4 +27,4 @@ def create_database(app):
     if not path.exists('instance/' + DB_NAME):
         with app.app_context():
             db.create_all()
-        print('Created database!')
+            print('Created database!')
