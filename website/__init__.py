75e47568b1fed135d2b9cd67322e0eecd651713d
anuragcar <akar01@nyit.edu>
2023-11-27 15:25:51 -0500

    add itinerary table for storing travel plans

--- website/__init__.py
+++ website/__init__.py
@@ -7,8 +7,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+class Itinerary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # one to many relationship, one user with many itineraries
+
 class User(db.Model, UserMixin):
-    email = db.Column(db.String(320), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(320), unique=True, nullable=False)
     password = db.Column(db.String(64))
     name = db.Column(db.String(150))
     
