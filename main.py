45b4b3db6383704e29c35b392294fc76e8d9358e
anuragcar <akar01@nyit.edu>
2023-12-01 19:47:26 -0500

    add comments

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from website.__init__ import create_app
 
-app = create_app()
+app = create_app() # create instance of flask app
 
-if __name__ == '__main__':
+if __name__ == '__main__': # check if script executed directly: python main.py
     app.run(debug=True)
+    
+'''
+WanderSync
+
+References for Python files
+Google SSO: https://www.youtube.com/watch?v=FKgJEfrhU1E
+Flask DB/Auth: https://youtu.be/dam0GPOAvVI
+'''
