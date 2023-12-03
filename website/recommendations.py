11354f476800d549632423bf97ceb24193acf98d
anuragcar <akar01@nyit.edu>
2023-12-03 18:20:27 -0500

    Added spaCy, a Natural Processing Language library

--- website/recommendations.py
+++ website/recommendations.py
@@ -1,10 +1,60 @@
 import googlemaps
+import spacy
 import requests
 from geopy.geocoders import Nominatim
-from .secret import GOOGLE_API_KEY
 
+GOOGLE_API_KEY = "AIzaSyB1VMCjcrzwJHEbVVCJ2FRuqWBcoGeoubM"
 map = googlemaps.Client(GOOGLE_API_KEY)
 geolocator = Nominatim(user_agent="place-locator")
+
+def filter_nouns_and_adjectives(text):
+    # Load the English language model
+    nlp = spacy.load("en_core_web_sm")
+
+    # Process the input text
+    doc = nlp(text)
+
+    # Define a list of specific adjectives (cuisines)
+    specific_adjectives = [
+        'american', 'italian', 'mexican', 'french', 'chinese', 'japanese', 'indian', 'thai', 'greek', 'mediterranean',
+        'spanish', 'korean', 'vietnamese', 'middle eastern', 'moroccan', 'turkish', 'lebanese', 'german', 'english',
+        'irish', 'african', 'ethiopian', 'nigerian', 'south african', 'caribbean', 'jamaican', 'cuban', 'latin american',
+        'brazilian', 'peruvian', 'argentinian', 'colombian', 'australian', 'indonesian', 'malaysian', 'filipino',
+        'russian', 'scandinavian', 'swedish', 'norwegian', 'danish', 'polish', 'turkish', 'mexican', 'spanish',
+        'arabic', 'israeli', 'georgian', 'pakistani', 'afghan', 'persian', 'iraqi', 'kurdish', 'austrian', 'swiss',
+        'belgian', 'portuguese', 'czech', 'hungarian', 'dutch', 'surinamese', 'sri lankan', 'tibetan', 'nepali', 'mongolian',
+        'cambodian', 'laotian', 'maldivian', 'bangladeshi', 'bhutanese', 'hawaiian', 'alaskan', 'polynesian', 'micronesian',
+        'caribbean', 'cuban', 'puerto rican', 'jamaican', 'trinidadian', 'haitian', 'dominican', 'bahamian', 'caymanian',
+        'central american', 'mexican', 'guatemalan', 'salvadoran', 'honduran', 'nicaraguan'
+    ]
+
+    # Filter out insignificant adjectives and keep the first relevant noun, the first relevant adjective,
+    # and specific adjectives from the user input
+    filtered_tokens = []
+
+    # Flags to track whether a noun and adjective have been added
+    noun_added = False
+    adjective_added = False
+
+    for token in doc:
+        if token.pos_ == 'NOUN' and token.text.lower() and not noun_added:
+            filtered_tokens.append(token.text.lower())
+            noun_added = True
+        elif token.pos_ == 'ADJ' and not adjective_added:
+            filtered_tokens.append(token.text.lower())
+            adjective_added = True
+
+    # Include specific adjectives from the user input
+    for token in doc:
+        if token.text.lower() in specific_adjectives:
+            filtered_tokens.append(token.text.lower())
+
+    # Join the filtered tokens to form the output string
+    output = ' '.join(filtered_tokens)
+
+    print(output)
+
+    return output
 
 def find_locations(location, radius=1000, keyword=None, types=None):
     location_coordinates = geolocator.geocode(location)
@@ -46,7 +96,8 @@ def recommend_locations(user_city, distance_filter, keyword):
 
     recommended_places_with_ratings = []
 
-    places = find_locations(user_city, radius=distance_filter, keyword=keyword, types=keyword)
+    preprocessed_text = filter_nouns_and_adjectives(keyword)
+    places = find_locations(user_city, radius=distance_filter, keyword=preprocessed_text, types=preprocessed_text)
 
     for place in places[0:5]:
         place_id = place.get('place_id')
@@ -59,7 +110,7 @@ def recommend_locations(user_city, distance_filter, keyword):
                 'photo_url': photo_url
             })
 
-    sorted_places = sorted(recommended_places_with_ratings, key=lambda x: float(x['rating']) if x['rating'] != 'N/A' else -1, reverse=True)
+    sorted_places = sorted(recommended_places_with_ratings, key=lambda x: float(x['rating']) if x['rating'] != 'N/A' else 0, reverse=True)
 
     return sorted_places
 
