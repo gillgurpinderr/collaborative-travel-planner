86fce30fc44dc5d8e584ccfca996507d52b79076
anuragcar <akar01@nyit.edu>
2023-12-02 21:03:42 -0500

    update models

--- website/models.py
+++ website/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
-class Itinerary(db.Model):
+class Itinerary(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # one to many relationship, one user with many itineraries
